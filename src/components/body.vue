388d371b19581184845236140fde80703f8f2242
Luka Dobrijevic <lukadob@Lukas-MacBook-Pro.local>
2020-09-16 17:33:47 -0600

    Created a simple site to practice making vue components

--- src/components/body.vue
+++ src/components/body.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="body-grid">
       <img width="300px" src="https://cdn.wallpapersafari.com/20/14/0N4Ik7.jpg" alt="Squidward">
-    <p>You like Krabby Patties, don't you Squidward...</p>
-    <img width="300px" src="https://i.pinimg.com/originals/b4/02/19/b40219b0e854f67221479a0d89909f84.jpg" alt="Spongebob">
+    <p>"You like Krabby Patties, don't you Squidward..."</p>
+    <img width="300px"  src="https://i.pinimg.com/originals/b4/02/19/b40219b0e854f67221479a0d89909f84.jpg" alt="Spongebob" >
+    <div id="sidePanel">
+      <new-sponge> </new-sponge>
+    </div>
   </div>
+  
+  
 </template>
 
 <script>
+import NewSponge from "./sidepanel.vue";
 export default {
-  name: "page-body"
+  name: "page-body",
+  components: {
+    NewSponge
+  }
 };
 </script>
 
@@ -17,6 +26,6 @@ export default {
         display: grid;
         justify-items: center;
         align-items: center;
-        grid-template-rows: 1fr 1fr 1fr;
+        grid-template-rows: 1fr 1fr 1fr 2fr;
     }
 </style>
--- src/components/sidepanel.vue
+++ src/components/sidepanel.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="newSponge">
-        <img width="200px" src="https://i.redd.it/cxmxo7yol5811.jpg" alt="spongeBob">
-        <img width="200px" src="https://memegenerator.net/img/images/15532021.jpg" alt="spongeBob">
+        <img id="jelly" width="200px" src="https://vignette.wikia.nocookie.net/spongebobgalaxy/images/1/11/King_J.jpg/revision/latest/scale-to-width-down/340?cb=20140916203740" alt="jellyFish">
+        <div></div>
+        <img id="bob" width="90px" src="https://i.redd.it/cxmxo7yol5811.jpg" alt="spongeBob">
     </div>
 </template>
 
@@ -12,5 +13,23 @@ export default {
 </script>
 
 <style scoped>
+    #newSponge {
+        display: grid;
+        justify-items: center;
+        align-items: center;
+        grid-template-columns: 1fr 0.5fr 1fr;
+        
+    }
 
+    #jelly {
+        display: grid;
+        justify-self: baseline;
+    }
+
+    #bob {
+        display: grid;
+        align-self: end;
+        justify-self: baseline;
+
+    }
 </style>
